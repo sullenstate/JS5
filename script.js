7b7c22ab981354e633d7cc99f08bd81bc8288f60
Tim <tklein@outlook.com>
2015-06-26 18:11:49 -0600

    Friday Final

--- script.js
+++ script.js
@@ -5,28 +5,26 @@
 
 $(function(){
 
-	$('.container > p').click(function(){
+	$('.editable').click(function(){
 
-		// Fade the text out
-		$('.container').children().hide();
+		var existingText = $(this).text();
+		$(this).hide();
 		// Append a text input block
-		$('.container').append('<textarea id="freeForm">');
-		$('.container').append('<div class="button buttonYes">Accept Changes</div>');
-		$('.container').append('<div class="button buttonNo">Cancel</div>');
+		$(this).parent().append('<textarea id="freeForm">');
+		$(this).parent().append('<div class="button buttonYes">Accept Changes</div>');
+		$(this).parent().append('<div class="button buttonNo">Cancel</div>');
+		$('#freeForm').attr("placeholder", existingText);
 		$('#freeForm').focus();
 	});
 
 	$('.container').on('click', '.button', function(){
 		if ($(this).text() === 'Accept Changes') {
-		$('.container').find('p').text($('#freeForm').val());
-		$('#freeForm, .button').remove();
-		$('.container > p').show();
+			$(this).siblings('p').text($('#freeForm').val());
+			$('#freeForm, .button').remove();
+			$('.container > p').show();
 		} else {
-		$('#freeForm, .button').remove();
-		$('.container > p').show();
+			$('#freeForm, .button').remove();
+			$('.container > p').show();
 		}
 	});
-
-	
-
 });
